3d84b3df0dd638c0239b42daee65dd667fca8bc5
unknown <nouralhosainy0@gmail.com>
2021-11-07 17:23:31 +0200

    routes changes

--- routes/api/flights.js
+++ routes/api/flights.js
@@ -1,44 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-// Load Flight model
 const Flight = require('../../models/Flight');
 
-// @route GET api/flights/test
-// @description tests flights route
-// @access Public
-router.get('/test', (req, res) => res.send('flight route testing!'));
-
-// @route GET api/flights
-// @description Get all flights
-// @access Public
+//Get all flights
 router.get('/', (req, res) => {
   Flight.find()
     .then(flights => res.json(flights))
     .catch(err => res.status(404).json({ noflightsfound: 'No Flights found' }));
 });
 
-// @route GET api/flights/:id
-// @description Get single flight by id
-// @access Public
+//Search for a flight
+router.put('/search', (req, res) => {
+	console.log(req.body)
+  Flight.find(req.body)
+    .then(flights => res.json(flights))
+    .catch(err => res.status(404).json({ noflightsfound: 'No Flights found' }));
+});
+
+
+//Get a single flight by id
 router.get('/:id', (req, res) => {
   Flight.findById(req.params.id)
     .then(flight => res.json(flight))
     .catch(err => res.status(404).json({ noflightfound: 'No Flight found' }));
 });
 
-// @route GET api/flights
-// @description add/save flight
-// @access Public
+//Create flight
 router.post('/', (req, res) => {
   Flight.create(req.body)
     .then(flight => res.json({ msg: 'Flight added successfully' }))
     .catch(err => res.status(400).json({ error: 'Unable to add this flight' }));
 });
 
-// @route GET api/flights/:id
-// @description Update flight
-// @access Public
+//Update flight
 router.put('/:id', (req, res) => {
   Flight.findByIdAndUpdate(req.params.id, req.body)
     .then(flight => res.json({ msg: 'Updated successfully' }))
@@ -47,9 +42,7 @@ router.put('/:id', (req, res) => {
     );
 });
 
-// @route GET api/flights/:id
-// @description Delete flight by id
-// @access Public
+//Delete flight by id
 router.delete('/:id', (req, res) => {
   Flight.findByIdAndRemove(req.params.id, req.body)
     .then(flight => res.json({ mgs: 'Flight entry deleted successfully' }))
