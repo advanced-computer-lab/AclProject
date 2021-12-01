be8a7628d3af5a22210e26dc32603fd86c792419
youssefmagdyy <youssefmagdy1910@gmail.com>
2021-12-01 07:27:15 +0200

    Profile design update

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import axios from 'axios';
+import loginPic from '../login.png';
+import Button from '@mui/material/Button';  
 import jwt from 'jsonwebtoken';
 
 class Profile extends Component {
@@ -38,81 +40,35 @@ class Profile extends Component {
   };
 
   render() {
-
-    const userinfo = this.state.userinfo;
-
-    let UserItem = <div>
-      <table className="table table-hover table-dark">
-        
-        <tbody>
-          <tr>
-            <th scope="row"></th>
-            <td>First Name</td>
-            <td>{userinfo.firstname}</td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td>Last Name</td>
-            <td>{userinfo.lastname}</td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td>Username</td>
-            <td>{userinfo.username}</td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td>Email</td>
-            <td>{userinfo.email}</td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td>Passport Number</td>
-            <td>{userinfo.passport}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
     return (
       <div className="Profile">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-10 m-auto">
-              <br />
-              <Link to="/show-flight-list" className="btn btn-outline-warning float-left">
-                  Flight List
-              </Link>
-              <Link to="/edit-profile" className="btn btn-outline-warning float-right">
-                Edit Profile
-              </Link>
-            </div>
-            <br />
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Profile Information</h1>
-              <p className="lead text-center">
-                  
-              </p>
-              <hr /> <br />
-            </div>
-          </div>
-          <div>
-            { UserItem }
-          </div>
-          <button onClick={event =>  window.location.href='/reservedflights'} type="button" class="btn btn-primary btn-lg btn-block">View Reserved Flights</button>
-
-
-          <div className="row">
-            <div className="col-md-6">
-            </div>
-
-            <div className="col-md-6">
-              <br />
-            </div>
-
-          </div>
-        </div>
+      <br />
+      <br />
+      <div className="backgroundLabelProfile">
+        <b >{this.state.userinfo.firstname + " " + this.state.userinfo.lastname}</b>
       </div>
+      <div className="backgroundBoxProfile">
+        <br />
+        <img src={loginPic} width="160px" height="160px" alt="fireSpot" />
+        <br />  
+        
+        <div className='profileInfo'>
+            <label htmlFor="username">{this.state.userinfo.username}</label>
+            <br />
+            <label htmlFor="email">{this.state.userinfo.email}</label>
+            <br />
+            <label htmlFor="passport">{"Passport: " + this.state.userinfo.passport}</label>
+             
+            </div>
+            
+            <Button variant="contained" style={{
+              width: "90px",
+              height: "30px",
+             
+            }} href="./edit-profile">Edit</Button>
+      </div>
+      <br />
+    </div>
     );
   }
 }
