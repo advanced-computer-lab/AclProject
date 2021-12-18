7d9bebc073ba6ba1027e8ea8e7c82f08f3ed4467
unknown <nouralhosainy0@gmail.com>
2021-12-18 23:38:34 +0200

    All login username entries are in lowercase

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,10 +7,10 @@ const User = require('../../models/User');
 
 router.get('/', (req, res) => {
 	User.find()
-	  .then(user => res.json(user))
-	  .catch(err => res.status(404).json({ nouserfound: 'No User found' }));
-	  
-  });
+		.then(user => res.json(user))
+		.catch(err => res.status(404).json({ nouserfound: 'No User found' }));
+
+});
 
 router.post('/registration', async (req, res) => {
 	console.log(req.body)
@@ -21,32 +21,33 @@ router.post('/registration', async (req, res) => {
 		const email = await User.findOne({
 			email: req.body.email.toLowerCase(),
 		})
-		
 
-	
+
+
 		if (user) {
 			return res.json("Username already taken")
 		}
-		if(email){
+		if (email) {
 
-     	return res.json("Another account is registered by this email")
+			return res.json("Another account is registered by this email")
 		}
 
-		else{
-				const newPassword = await bcrypt.hash(req.body.password, 10)
-		await User.create({
-			username: req.body.username.toLowerCase(),
-			firstname: req.body.firstname,
-			lastname: req.body.lastname,
-			email: req.body.email.toLowerCase(),
-			address: req.body.address,
-			countrycode: req.body.countrycode,
-			telenumber1: req.body.telenumber1,
-			telenumber2: req.body.telenumber2,
-			telenumber3: req.body.telenumber3,
-			password: newPassword,
-			passport: req.body.passport,
-		})}
+		else {
+			const newPassword = await bcrypt.hash(req.body.password, 10)
+			await User.create({
+				username: req.body.username.toLowerCase(),
+				firstname: req.body.firstname,
+				lastname: req.body.lastname,
+				email: req.body.email.toLowerCase(),
+				address: req.body.address,
+				countrycode: req.body.countrycode,
+				telenumber1: req.body.telenumber1,
+				telenumber2: req.body.telenumber2,
+				telenumber3: req.body.telenumber3,
+				password: newPassword,
+				passport: req.body.passport,
+			})
+		}
 		return res.json("User Created")
 	} catch (err) {
 		res.json({ status: 'error', error: 'Duplicate email' })
@@ -54,9 +55,16 @@ router.post('/registration', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-	const user = await User.findOne({
-		username: req.body.username.toLowerCase(),
-	})
+	var user;
+	if (req.body.username === "Administrator" || req.body.username === "administrator")
+		user = await User.findOne({
+			username: "Administrator",
+		})
+	else {
+		user = await User.findOne({
+			username: req.body.username.toLowerCase(),
+		})
+	}
 
 	if (!user) {
 		return res.json("User not found")
@@ -88,41 +96,40 @@ router.post('/login', async (req, res) => {
 router.put('/:id', (req, res) => {
 
 	User.findByIdAndUpdate(req.params.id, req.body)
-	  .then(user => res.json({ msg: 'Updated successfully' }))
-	  .catch(err =>
-		res.status(400).json({ error: 'Unable to update the Database' })
-	  );
-  });
+		.then(user => res.json({ msg: 'Updated successfully' }))
+		.catch(err =>
+			res.status(400).json({ error: 'Unable to update the Database' })
+		);
+});
 
 router.put('/password/:id', async (req, res) => {
 
 	const newPassword = await bcrypt.hash(req.body.password, 10)
-	var datapassword ={password: newPassword}
+	var datapassword = { password: newPassword }
 
 	User.findByIdAndUpdate(req.params.id, datapassword)
-	  .then(user => res.json({ msg: 'Updated successfully' }))
-	  .catch(err =>
-		res.status(400).json({ error: 'Unable to update the Database' })
-	  );
-  });
+		.then(user => res.json({ msg: 'Updated successfully' }))
+		.catch(err =>
+			res.status(400).json({ error: 'Unable to update the Database' })
+		);
+});
 
 router.post('/:id', async (req, res) => {
-	try{
+	try {
 		const user = await User.findById(req.params.id)
 		const isPasswordValid = await bcrypt.compare(
 			req.body.oldpassword,
 			user.password
 		)
-		if(isPasswordValid)
-		{
+		if (isPasswordValid) {
 			return res.json('Match')
 		}
-	
+
 	}
 	catch (err) {
 		res.json({ status: 'error', error: 'Error Old Password' })
 	}
-  });
+});
 
 
 
